b1dd5ed4b1c5ae75b1ec6a56b8b1a93d1637d978
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-15 05:13:20 -0700

    finished, but for some reason my answers are wrong

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const ftoc = function() {
-
+const ftoc=(f)=>{
+  const conversion=(f-32)*(5/9);
+  if(conversion%1!==0){
+    return `${conversion.toFixed(1)}.`
+  }else{
+    return `${conversion}.`
+  }
 }
-
-const ctof = function() {
-
+const ctof =(c)=>{
+  const conversion=(c*(9/5))+32;
+  if(conversion%1!==0){
+    return `${conversion.toFixed(1)}.`
+  }else{
+    return `${conversion}.`
+  }
 }
-
 module.exports = {
   ftoc,
   ctof
