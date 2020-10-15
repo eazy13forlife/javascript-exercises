86e334180f5f77657f380e01400671b5c279f1bf
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-15 03:33:28 -0700

    changed to a simpler function that works in all cases

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-
+/*
 const removeFromArray=(originalArray,...things)=>{
   //we will create a for loop. we want to go through every item in our originalArray and find the index where an item there equals an item, i in our things array. We want to start from index 0 of the things array and work our way up to the last index of the things array. If condition is true, we increment things by 1.
   for(let i=0;i<=things.length-1;i++){
@@ -15,5 +15,17 @@ const removeFromArray=(originalArray,...things)=>{
   //return the array
   return originalArray;
 }
-console.log(removeFromArray(["cat","dog","apple"],"cat","apple"));
+
+README.md
+*/
+
+
+const removeFromArray=(...args)=>{
+  //get the first item in our args array and set it equal to firstArray;
+  const firstArray=args[0];
+  //use filter method on our firstArray. Look at each item and filter out the ones in which args array does NOT include that item.(because if args array includes the item, it means we dont want it.)
+  return firstArray.filter((item)=>!args.includes(item))
+}
+
+console.log(removeFromArray(["cat","dog","apple",7,,8],"cat","apple",7));
 module.exports = removeFromArray
