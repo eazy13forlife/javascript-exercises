8fb2c6b4383c8a5770e7f00bb88b659b30e8a10c
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-15 04:02:17 -0700

    finalized with comments

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,17 +1,21 @@
 const sumAll =(num1,num2)=> {
+  //if any of the conditions we don't want exist, return error right away
   if(num1<0||num2<0||typeof num1!=="number"||typeof num2!=="number"){
     return `ERROR`
   }
   let num=0;
+  //if num1<num2, run the for loop below, which just adds i to num
   if(num1<num2){
     for(let i=num1;i<=num2;i++){
       num+=i;
     }
+  //if num1>num2 or they are both the same, run the for loop below
   }else{
     for(let i=num2;i<=num1;i++){
       num+=i;
     }
   }
+  //return num
   return num;
 }
 console.log(sumAll(-6,0));
