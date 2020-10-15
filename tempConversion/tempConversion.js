6484ff38e7cdd971341050c2deff1fe37b232270
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-15 05:26:05 -0700

    finalized version, but for some reason, it still doesn't pass

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,17 +1,18 @@
 const ftoc=(f)=>{
   const conversion=(f-32)*(5/9);
   if(conversion%1!==0){
-    return `${conversion.toFixed(1)}.`
+    return conversion.toFixed(1);
+
   }else{
-    return `${conversion}.`
+    return conversion;
   }
 }
 const ctof =(c)=>{
   const conversion=(c*(9/5))+32;
   if(conversion%1!==0){
-    return `${conversion.toFixed(1)}.`
+    return conversion.toFixed(1);
   }else{
-    return `${conversion}.`
+    return conversion;
   }
 }
 module.exports = {
