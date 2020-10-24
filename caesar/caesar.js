b5067e9e03842eb4c6dfb17cdb0f3b7ef1a5259d
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-23 21:39:18 -0700

    finished some of it, works with single letters, words, punctuation, but not capitalization and wrap

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,41 @@
-const caesar = function() {
-
+const caesar=(word,number)=>{
+  let emptyString=""
+  const letterOrderingArray=[]
+  const alphabet="abcdefghijklmnopqrstuvwxyz"
+  const alphabetArray=alphabet.split("");
+  for(let i=0;i<=alphabetArray.length-1;i++){
+    letterOrderingArray.push({
+      letter:alphabetArray[i],
+      value:i+1,
+    })
+  }
+  console.log(letterOrderingArray);
+  const wordArray=word.split("");
+  console.log(wordArray)
+  outer:for(let i=0;i<=wordArray.length-1;i++){
+    inner:for(let j=0;j<=letterOrderingArray.length;j++){
+      //if letter in our word is equal to the letter in our alphabet, which means the other 26 letters there was no match, otherwise the next iteration in our for loop would have began
+      if(j===letterOrderingArray.length){
+        emptyString+=wordArray[i];
+      }else{
+        if(wordArray[i]===letterOrderingArray[j].letter){
+          //find the value associated with that letter
+          const value=letterOrderingArray[j].value;
+          //add that value to the number argument to get the total number we should be in our alphabet
+          const totalNumber=value+number;
+          //go to our letterOrderingArray and find where the letters value property equals totalNumber
+          const letterObject=letterOrderingArray.find((letter)=>letter.value===totalNumber)
+          //find that letter and add it to the emptrystring
+          const caesarLetter=letterObject.letter;
+          emptyString+=caesarLetter;
+          //after we added this caesarLetter to our empty string, we continue with the next iteration of our outer loop, which looks at the next word in our wordArray.
+          continue outer;
+        }
+      }
+    }
+  }
+  return emptyString;
 }
+
 
 module.exports = caesar
