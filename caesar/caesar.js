9d60495809acb22d13087762fc0889a75fa0f218
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-27 07:58:48 -0700

    finished comments as well

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -23,7 +23,7 @@ const caesar=(word,number)=>{
           const value=letterOrderingArray[j].value;
           //add that value to the number argument to get the total number we should be in our alphabet
           let totalNumber=value+number;
-          //if totalNumber is greater than 26, then the new totalNumber is the old totalNumber divided by 26. The remainder will be the new totalNumber,this will allow us to continue repeat a-z/wrap around the alphabet. For example, if totalNumber is 53, then thats twice fully and one remainder, so that leaves us at letter a.
+          //if totalNumber is less than or equal to 0, this is what we want to happen. if a is 1 and we move -1, the totalNumber is 0. 0 should be z. if we move -2, the totalNumber is -1 and -1 should be y. If we move -3, the totalNumber is -2 and -2 should be x. Hence, if we are starting at 0 for a value, 0-25 is indeed 26 letters in the alphabet,so that means a's value is -25. So we recreate our letterOrderingArray when we get a negative totalNumber. So when the letter ordering is done, letter z, which is alphabetArray[25] has a value of 0, letter y has a value of -1, letter x has -2 and a has a value of -25.
           if(totalNumber<=0){
             letterOrderingArray=[];
             for(let i=0;i<=alphabetArray.length-1;i++){
@@ -34,7 +34,7 @@ const caesar=(word,number)=>{
             }
             console.log(letterOrderingArray)
           }
-
+          //if totalNumber is greater than 26 or less than -26, then the new totalNumber is the old totalNumber divided by 26. The remainder will be the new totalNumber,this will allow us to continue repeat a-z/wrap around the alphabet. For example, if totalNumber is 53, then thats twice fully around the alphabet, because 26+26 is 52. and one remainder, so that leaves us at letter a.
           if(totalNumber>26||totalNumber<(-26)){
             totalNumber%=26
           }
