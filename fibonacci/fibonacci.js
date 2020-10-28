45a4ce36817bf950469c533eba4ae777d6038ac4
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-27 23:59:21 -0700

    finished fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = (number)=> {
+  number=+number
+  if(number<=0){
+    return "OOPS";
+  }else{
+    let fibArray=[0,1]
+    for(let i=2;i<=number;i++){
+      fibArray[i]=fibArray[i-1]+fibArray[i-2];
+    }
+    return fibArray[number];
+  }
 }
+
+
 
 module.exports = fibonacci
