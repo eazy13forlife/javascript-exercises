dd8d96f5ee1b99fb19066c8ae57c609e76fec5af
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-28 00:28:45 -0700

    finsihed with comments as well

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  //use map method which creates a new array based on the return value of the callback function. Our callback function here looks at each book in the array and returns book.title property
+  return array.map((book)=>book.title)
 }
-
 module.exports = getTheTitles;
